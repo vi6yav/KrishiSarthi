b05bfc056e0ca14f6722b2871c39249c04b676db
vi6yav <vvermakol@gmail.com>
2026-08-28 18:41:31 +0530

    Add production frontend URL to CORS allowlist

--- api/app/main.py
+++ api/app/main.py
@@ -17,7 +17,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://krishisarthivv.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
